eaf2c917825244db04184139d62ed96140ce024b
jon <jon.kleehammer@lease-analytics.com>
2023-05-29 18:18:09 -0400

    removed actionCable.js, I think it makes more sense to have each screen connect to ActionCable

--- src/components/LobbyScreen.vue
+++ src/components/LobbyScreen.vue
@@ -7,30 +7,35 @@
 </template>
 
 <script setup>
-import { onMounted, onUnmounted, ref } from "vue";
-import { connectActionCable, disconnectActionCable } from "@/actionCable"
+import { ref } from "vue";
 import { useRoute } from "vue-router"
 import { useSessionStore } from "@/stores/sessionStore";
 import Button from "primevue/button";
+import {createConsumer} from "@rails/actioncable";
 
-
+// ActionCable Connection
 const route = useRoute()
 const lobbyCode = route.params.lobbyCode
-
 const sessionStore = useSessionStore()
 const userID = sessionStore.userID
-
-onMounted(() => {
-  connectActionCable(lobbyCode, userID)
-})
-
-onUnmounted(() => {
-  disconnectActionCable()
+const cable = createConsumer('ws://localhost:3000/cable')
+const connection = cable.subscriptions.create({ channel: 'LobbyChannel', lobby_code: lobbyCode, user: userID }, {
+  connected() {
+    console.log('ActionCable connected')
+  },
+  disconnected() {
+    console.log('ActionCable disconnected')
+  },
+  received(data) {
+    console.log(data)
+    if (data === 'DATA')
+      testArr.value.push(1)
+  }
 })
 
 const testArr = ref([])
 const test = () => {
-  testArr.value.push(0)
+  connection.perform('test_action', { testKey: 'testValue' })
 }
 </script>
 
