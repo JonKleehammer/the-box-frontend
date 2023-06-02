3394814fbfcb07bc27338e0378dbd148624ddf03
jon <jon.kleehammer@lease-analytics.com>
2023-06-02 17:42:19 -0400

    working on the Question/Answer card and got it scaffolded out

--- src/components/Games/TriviaTussle/AnswerQuestion.vue
+++ src/components/Games/TriviaTussle/AnswerQuestion.vue
@@ -1,9 +1,26 @@
 <template>
-  <h1>QUESTION</h1>
+<Card>
+  <template #subtitle>
+    {{ gameState.topic }} for 100 points! PLAYER Asks...
+  </template>
+  <template #content>
+    {{ gameState.question }}
+  </template>
+  <template #footer>
+    <div class="p-inputgroup flex-1">
+      <InputText/>
+      <Button label="submit"/>
+    </div>
+  </template>
+</Card>
 </template>
 
 <script setup>
+import Card from 'primevue/card'
+import InputText from "primevue/inputtext";
+import Button from 'primevue/button'
 
+const { gameState } = defineProps(['gameState'])
 </script>
 
 <style scoped>
